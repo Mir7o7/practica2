66a3ffa6023cfe5713f9b9d7e9f64dacd00d5b1b
Mir7o7 <gilberth.concepcion@tecsup.edu.pe>
2025-05-05 16:08:08 -0500

    Update db.js

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const { Pool } = require('pg');
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  connectionString: process.env.postgresql://dbpractica2_user:MPvHcGRV9nVZz1cBflPlZ9vACe86aApy@dpg-d0cgu06mcj7s738f5310-a.oregon-postgres.render.com/dbpractica2,
 });
 
 module.exports = pool;
